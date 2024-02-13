5d7d591eb3a983dc5616a3ab272347b580ed98b2
ismartNishant <nishantrajputall@gmail.com>
2024-02-13 14:28:48 +0530

    ui fixed

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,20 +1,20 @@
 import type { FC } from 'react';
-
+import Product1 from "../assets/product1.jpg"
 interface CardProps { }
 
 const Card: FC<CardProps> = () => {
     return (
         <div className="max-w-sm mx-auto my-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
+            <a href="/" className='h-64'>
+                <img className="p-4 rounded-t-lg h-80 w-full object-cover" src={Product1} alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
-                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                    <h5 className="text-lg font-semibold tracking-tight text-gray-900 dark:text-white">
                         Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
                     </h5>
                 </a>
-                <div className="flex items-center mt-2.5 mb-5">
+                <div className="flex items-center mt-2.5 mb-2">
                     <svg
                         className="w-4 h-4 text-yellow-300 mr-1"
                         aria-hidden="true"
@@ -60,15 +60,15 @@ const Card: FC<CardProps> = () => {
                     >
                         <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                     </svg>
-                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+                    <span className="bg-blue-100 text-indigo-500 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-indigo-500 ml-3">
                         4.0
                     </span>
                 </div>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                    <span className="text-xl font-bold text-gray-900 dark:text-white">$599</span>
                     <a
                         href="/"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        className="text-white bg-indigo-500 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
                         Add to cart
                     </a>
